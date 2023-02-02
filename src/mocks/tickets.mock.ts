02c07be745f427c5f3a9a452d1f2a046f3ab87c0
Laurine FIGUEIRA <figueiralaurine@gmail.com>
2023-02-02 14:59:19 +0100

    first partie

--- src/mocks/tickets.mock.ts
+++ src/mocks/tickets.mock.ts
@@ -4,15 +4,17 @@ const dateToday: Date = new Date();
 
 export const TICKETS_MOCKED: Ticket[] = [
   {
-    title: 'SI4 in Madrid',
+    title: 'SI4 in Barcelone',
     description: '',
     date: dateToday,
-    author: 'Paul'
+    student: 'Paul',
+    major: 'SI',
   },
   {
     title: 'SI5 in Paris',
     description: 'Description du voyage',
     date: dateToday,
-    author: 'Anakin'
+    student: 'Anakin',
+    major: 'GE',
   },
 ];
